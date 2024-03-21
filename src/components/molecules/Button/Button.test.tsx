a9526516b06f160f29ff4f581c3e6d158786de94
Luane Aquino <luane.aquino.br@gmail.com>
2024-03-21 14:15:01 +0900

    test: configured vitest

--- src/components/molecules/Button/Button.test.tsx
+++ src/components/molecules/Button/Button.test.tsx
@@ -1,3 +1,5 @@
+import { render, screen, userEvent } from "../../../../test/test-utils";
+import Button from "./";
 /*
 - should render button with text
 */
@@ -6,5 +8,7 @@
 //   render();
 // });
 test("that jest is working", () => {
+  render(<Button text="hello" />);
   expect(true).toBe(true);
+  expect(screen.getByText("hello")).toBeVisible();
 });
